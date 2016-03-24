70abb5b74a3bdf59785436192bcb972804c32074
hrvojetvbeat <hrvoje@tvbeat.com>
2016-03-24 20:55:14 +0100

    feeds_store manage command done

--- rss/rss_collector/management/commands/feeds_store.py
+++ rss/rss_collector/management/commands/feeds_store.py
@@ -35,32 +35,3 @@ class Command(BaseCommand):
                         p = Feeds(sources=s, title=t, publish_time=pd, link=l, author=a, img_url=i)
                         p.save()
         self.stdout.write("Feeds table updated")
-
-
-                    # print(source)
-                    # print(value['title'])
-                    # print(value['link'])
-                    # print(value['author'])
-                    # print(datetime.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(value['published'])), zagreb))
-                    # dd = datetime.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(value['published'])), zagreb)
-                    # print(value['img'])
-                    # posts.append(value)
-        # dt = datetime.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz("Thu, 24 Mar 2016 16:32:01 +0100")), zagreb)
-        # p = Feeds(sources=Sources.objects.get(pk=1), title="Kamion je istovarivao zemlju i pregazio vlasnika kuće", publish_time=dt, link="http://www.24sata.hr/news/nesreca-kamion-je-istovarivao-zemlju-i-pregazio-vlasnika-kuce-466767", author="Željko Rukavina", img_url="http://www.24sata.hr/media/img/e8/cb/42bf6010749bc9cac464.jpeg")
-        # p.save()
-
-
-
-
-
-        # for feeds_id in options['feeds_id']:
-
-            # try:
-            #     poll = Poll.objects.get(pk=poll_id)
-            # except Poll.DoesNotExist:
-            #     raise CommandError('Poll "%s" does not exist' % poll_id)
-
-            # poll.opened = False
-            # poll.save()
-
-            # self.stdout.write(self.style.SUCCESS('Feeds successfully updated "%s"' % feed_id))
